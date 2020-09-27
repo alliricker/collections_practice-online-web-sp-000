8daa14aeb77b01a9e15266c51b03661903c7fbab
Allison Ricker <alliricker@gmail.com>
2020-09-27 20:06:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,14 +26,12 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.each do |i|
-    i[2] = "$"
+  array.each do {|i| i[2] = "$"}
   end
 end
 
 def find_a(array)
-  array.find_all do |letter|
-    letter[0] == "a"
+  array.find_all do {|letter| letter[0] == "a"}
   end
 end
 
