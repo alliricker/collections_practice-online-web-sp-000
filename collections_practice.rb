9379c633271774f21f4f433842017a2e0cdfc562
Allison Ricker <alliricker@gmail.com>
2020-09-27 20:07:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,12 +4,15 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort! do {|a, b| b <=> a}
+  array.sort! do |a, b|
+    b <=> a
    end
 end
 
 def sort_array_char_count(array)
-  array.sort do {|a, b| a.length <=> b.length}
+  array.sort do
+    |a, b|
+    a.length <=> b.length
   end
 end
 
